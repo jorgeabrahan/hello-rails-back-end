837cef19c30a1a2b6ca448f07687f9a97ba81b06
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-06 23:28:56 -0600

    Setup route for the random_greeting endpont

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/random_greeting', to: 'greeting#random_greeting'
 
   # Defines the root path route ("/")
   # root "articles#index"
